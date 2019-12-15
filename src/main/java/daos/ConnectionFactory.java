00c29371ad2ea18795a79e42da780f1d76520a85
eddie <eddiem9657@gmail.com>
2019-12-15 17:09:38 -0500

    done

---
+++ src/main/java/daos/ConnectionFactory.java
@@ -0,0 +1,64 @@
+package daos;
+import models.Car;
+import java.sql.*;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
+public class ConnectionFactory {
+    public static final String URL = "jdbc:postgresql://localhost:5432/cars";
+    public static final String USER = "chung";
+    public static final String PASS = "chung";
+    public static Connection getConnection(){
+        try{
+            DriverManager.registerDriver(new Driver() {
+                @Override
+                public Connection connect(String url, Properties info) throws SQLException {
+                    return null;
+                }
+                @Override
+                public boolean acceptsURL(String url) throws SQLException {
+                    return false;
+                }
+                @Override
+                public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+                    return new DriverPropertyInfo[0];
+                }
+                @Override
+                public int getMajorVersion() {
+                    return 0;
+                }
+                @Override
+                public int getMinorVersion() {
+                    return 0;
+                }
+                @Override
+                public boolean jdbcCompliant() {
+                    return false;
+                }
+                @Override
+                public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+                    return null;
+                }
+            });
+            return DriverManager.getConnection(URL, USER, PASS);
+        }catch(SQLException e){
+            throw new RuntimeException("Error Connecting to the database", e);
+        }
+    }
+    public static void main(String[] arg){
+        DataAccessor dao = new DataAccessor();
+        System.out.println("starting");
+        try{
+            List<Car> cars = dao.findAll();
+            for(Car each : cars){
+                System.out.println(each.toString() + "nothing");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+            System.out.println("not working");
+        }
+    }
+}
