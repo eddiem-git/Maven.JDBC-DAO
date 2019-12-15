8b1494f7b787deae3e6296f025c77a5dad215611
eddie <eddiem9657@gmail.com>
2019-12-15 17:10:51 -0500

    Main method

--- src/main/java/daos/ConnectionFactory.java
+++ src/main/java/daos/ConnectionFactory.java
@@ -48,17 +48,4 @@ public class ConnectionFactory {
             throw new RuntimeException("Error Connecting to the database", e);
         }
     }
-    public static void main(String[] arg){
-        DataAccessor dao = new DataAccessor();
-        System.out.println("starting");
-        try{
-            List<Car> cars = dao.findAll();
-            for(Car each : cars){
-                System.out.println(each.toString() + "nothing");
-            }
-        }catch(SQLException e){
-            e.printStackTrace();
-            System.out.println("not working");
-        }
-    }
 }
--- src/main/java/daos/Main.java
+++ src/main/java/daos/Main.java
@@ -1,4 +1,22 @@
 package daos;
 
+import models.Car;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] arg){
+        DataAccessor dao = new DataAccessor();
+        System.out.println("starting");
+        try{
+            List<Car> cars = dao.findAll();
+            for(Car each : cars){
+                System.out.println(each.toString() + "nothing");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+            System.out.println("not working");
+        }
+    }
 }
